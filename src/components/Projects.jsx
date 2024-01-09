3a6d0a9e171a633c670638cfcf42afb95f5c0e89
“elisejonkers” <elisejonkers18@hotmail.com>
2024-01-09 16:24:49 +0100

    getting project data and display in component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,40 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 
+
 function Projects () {
+const [projects, setProjects] = useState([])
+
+const loadProjects = () => {
+  axios
+    .get(`${import.meta.env.VITE_API_URL}/projects`)
+    .then((response) => {
+      console.log("this is projects", response.data);
+      setProjects(response.data);
+    })
+    .catch((error) => {
+      console.log("This is error", error);
+    });
+};
+
+useEffect(() => {
+    loadProjects()
+}, [])
+
     return (
         <div>
-        <h1>This is Projects</h1>
+        {projects.map((project) => {
+            return (
+                <div>
+                    <h3>{project.name}</h3>
+                    <p>{project.description}</p>
+                    <Link to={"/projectdetails/:projectId"}>
+                    <button className="btn btn-dark">More Details</button>
+                    </Link>
+                </div>
+            )
+        })}
         </div>
     )
 }

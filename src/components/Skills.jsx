bf2f74613736923d6641a9ab5e65acc411542eb7
“elisejonkers” <elisejonkers18@hotmail.com>
2024-01-11 19:32:34 +0100

    displaying skills in frontend

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,12 +1,46 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 
 function Skills () {
-    return (
-        <div>
-            <h1>This is Skills</h1>
-        
-        </div>
-    )
+const [skills, setSkills] = useState([])
+
+const loadSkills = () => {
+    axios
+    .get(`${import.meta.env.VITE_API_URL}/skills`)
+    .then((response) => {
+        setSkills(response.data)
+    })
+    .catch((error) => {
+        console.log("This is error in getting skills", error)
+    })
+}
+
+const typeOfSkill = (skill) => {
+    if (skill.type === "hard") {
+        return <span className="hard">{skill.name}</span>
+    } else if (skill.type === "soft") {
+        return <span className="soft">{skill.name}</span>
+    }
+}
+
+useEffect(() => {
+    loadSkills()
+}, [])
+
+return (
+    <div>
+        {skills.map((skill) => {
+            return (
+                <div>
+                    <p>{typeOfSkill(skill)}</p> 
+                </div>
+            )
+        })}
+    </div>
+)
+    
+
 }
 
 export default Skills

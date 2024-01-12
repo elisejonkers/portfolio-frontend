446cf306e0299d17a04a327e9f24bc1f4a2d0db3
“elisejonkers” <elisejonkers18@hotmail.com>
2024-01-12 12:45:14 +0100

    added components to HomePage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,44 @@
+import { useEffect } from "react";
+import AboutMe from "./AboutMe"
+import Education from "./Education";
+import Experience from "./Experience";
+import Projects from "./Projects"
+import Skills from "./Skills"
+
 function HomePage () {
-    return <h1>This is HomePage</h1>
+
+useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const sectionId = hash.slice(1);
+      const section = document.getElementById(sectionId);
+      if (section) {
+        section.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, []);
+
+
+    return (
+      <div>
+        <section id="aboutMe">
+          <AboutMe />
+        </section>
+        <section id="projects">
+          <Projects />
+        </section>
+        <section id="skills">
+          <Skills />
+        </section>
+        <section id="education">
+            <Education />
+        </section>
+        <section id="experience">
+            <Experience />
+        </section>
+      </div>
+    );
 }
 
 export default HomePage
+

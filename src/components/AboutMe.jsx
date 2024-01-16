2094c527efb77d23e7090df486aa78a1719b11c5
“elisejonkers” <elisejonkers18@hotmail.com>
2024-01-16 16:50:37 +0100

    added first styling and content to aboutme section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,12 +1,57 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import elise_1 from "../assets/IMGL0535.jpg";
+import elise_2 from "../assets/IMGL0552.jpg";
+import cvFile from "../assets/EliseJonkers_FullStackDeveloper_Resume.pdf"
 
-function AboutMe () {
-    return (
-        <div>
-            <h1>This is AboutMe</h1>
-        
+function AboutMe() {
+  const downloadCV = () => {
+    window.open(cvFile, "_blank");
+  };
+
+  return (
+    <div>
+      <div className="aboutme-div">
+        <div className="aboutme-item-1">
+          <img  src={elise_1} alt="photo 1" />
+          <button className="btn btn-sm bg-warning" onClick={downloadCV}>
+            Download my CV
+          </button>
         </div>
-    )
+        {/* <div className="aboutme-item-3">
+          
+        </div> */}
+        <div className="aboutme-item-2">
+          <h1>About me</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            sodales sit amet justo quis tempor. Phasellus egestas orci sed erat
+            tempus bibendum. Duis vel sollicitudin lacus, ut tristique lectus.
+            Sed egestas viverra rhoncus. Suspendisse neque nisi, malesuada eu
+            viverra sit amet, egestas tempor urna. Vestibulum nec augue quis
+            enim mattis mattis et sagittis sem. Morbi ornare enim felis, vitae
+            interdum ante viverra quis. Ut viverra, libero id vestibulum
+            malesuada, sapien orci egestas lacus, vitae dapibus odio tortor vel
+            elit. Nunc vitae ornare velit, eget dignissim leo. Cras maximus et
+            tellus id pellentesque.{" "}
+          </p>
+          <br></br>
+          <p>
+            Maecenas quis ex porttitor, efficitur nisi quis, euismod mi. Donec
+            tristique eros id nunc feugiat ultricies. Proin pellentesque
+            venenatis commodo. Sed dictum pretium metus. Proin id sapien
+            scelerisque, condimentum tortor ac, tincidunt augue. Phasellus ac
+            diam odio. Cras elementum ipsum felis, sit amet eleifend dolor
+            blandit vel. Aliquam mollis, risus ac luctus porta, quam risus
+            consequat tellus, eget faucibus felis nulla consequat magna.
+            Maecenas a semper urna. Sed et sem sapien. Nam ornare pharetra erat,
+            ut tincidunt justo imperdiet vitae. Cras pretium, dui eget viverra
+            viverra, magna nunc ultricies turpis, placerat aliquet felis quam
+            vitae magna. Duis accumsan elit vel maximus venenatis
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default AboutMe
+export default AboutMe;
--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -1,41 +1,54 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { Link, useParams } from "react-router-dom"
-useParams
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+useParams;
 
-function ProjectDetails () {
-const [project, setProject] = useState({})
-const {projectId} = useParams()
+function ProjectDetails() {
+  const [project, setProject] = useState({});
+  const { projectId } = useParams();
 
-const loadDetails = () => {
+  const loadDetails = () => {
     axios
-    .get(`${import.meta.env.VITE_API_URL}/projects/${projectId}`)
-    .then((response) => {
-        setProject(response.data)
+      .get(`${import.meta.env.VITE_API_URL}/projects/${projectId}`)
+      .then((response) => {
+        setProject(response.data);
+      })
+      .catch((error) => {
+        console.log("Error getting project details", error);
+      });
+  };
 
-    })
-    .catch((error) => {
-        console.log("Error getting project details", error)
-    })
-}
-console.log(project.technologies)
+  useEffect(() => {
+    loadDetails();
+  }, [projectId]);
 
-useEffect(() => {
-    loadDetails()
-}, [projectId])
-
-    return (
-        <div>
-            <a href={project.URL}>
-            <h2>{project.name}</h2>  
-            </a>
-            <p>{project.description}</p>     
-            
-                {/* {project.technologies.map((technology) => {
-                    return <p>{technology}</p>
-                })} */}
+  return (
+    <div className="card lg:card-side bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+          alt="Album"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{project.name}</h2>
+        <p>Click the button to listen on Spotiwhy app.</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Listen</button>
         </div>
-    )
+      </div>
+    </div>
+    // <div>
+    //     <a href={project.URL}>
+    //     <h2>{project.name}</h2>
+    //     </a>
+    //     <p>{project.description}</p>
+
+    //         {/* {project.technologies.map((technology) => {
+    //             return <p>{technology}</p>
+    //         })} */}
+    // </div>
+  );
 }
 
-export default ProjectDetails
+export default ProjectDetails;

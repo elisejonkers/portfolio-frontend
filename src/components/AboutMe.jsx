012c55b938c2c3769f5f1911f1f6bbf012c07ad0
“elisejonkers” <elisejonkers18@hotmail.com>
2024-01-17 16:45:48 +0100

    updated about me text

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import elise_1 from "../assets/foto1.jpg";
-import cvFile from "../assets/EliseJonkers_FullStackDeveloper_Resume.pdf"
+import cvFile from "../assets/EliseJonkers_FullStackDeveloper_Resume.pdf";
 
 function AboutMe() {
   const downloadCV = () => {
@@ -11,7 +11,7 @@ function AboutMe() {
     <div>
       <div className="aboutme-div">
         <div className="aboutme-item-1">
-          <img  src={elise_1} alt="photo 1" />
+          <img src={elise_1} alt="photo 1" />
           <button className="btn btn-sm bg-warning" onClick={downloadCV}>
             Download my CV
           </button>
@@ -22,31 +22,24 @@ function AboutMe() {
         <div className="aboutme-item-2">
           <h1>About me</h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-            sodales sit amet justo quis tempor. Phasellus egestas orci sed erat
-            tempus bibendum. Duis vel sollicitudin lacus, ut tristique lectus.
-            Sed egestas viverra rhoncus. Suspendisse neque nisi, malesuada eu
-            viverra sit amet, egestas tempor urna. Vestibulum nec augue quis
-            enim mattis mattis et sagittis sem. Morbi ornare enim felis, vitae
-            interdum ante viverra quis. Ut viverra, libero id vestibulum
-            malesuada, sapien orci egestas lacus, vitae dapibus odio tortor vel
-            elit. Nunc vitae ornare velit, eget dignissim leo. Cras maximus et
-            tellus id pellentesque.{" "}
+            Hi! I'm Elise, a Full Stack Developer. I recently changed careers
+            from finance to development, facilitated by a web development
+            Bootcamp by Ironhack. This decision was made to make better use of
+            my qualities as detail-oriented, problem-solving thinking,
+            creativity and persistance. My excitement about development grows
+            every day and I can't wait to turn this into professional
+            experience.
           </p>
-          <br></br>
+          <br />
           <p>
-            Maecenas quis ex porttitor, efficitur nisi quis, euismod mi. Donec
-            tristique eros id nunc feugiat ultricies. Proin pellentesque
-            venenatis commodo. Sed dictum pretium metus. Proin id sapien
-            scelerisque, condimentum tortor ac, tincidunt augue. Phasellus ac
-            diam odio. Cras elementum ipsum felis, sit amet eleifend dolor
-            blandit vel. Aliquam mollis, risus ac luctus porta, quam risus
-            consequat tellus, eget faucibus felis nulla consequat magna.
-            Maecenas a semper urna. Sed et sem sapien. Nam ornare pharetra erat,
-            ut tincidunt justo imperdiet vitae. Cras pretium, dui eget viverra
-            viverra, magna nunc ultricies turpis, placerat aliquet felis quam
-            vitae magna. Duis accumsan elit vel maximus venenatis
+            During the Ironhack Bootcamp I have gained several skills to create
+            dynamic and interesting web applications. Combined with my previous
+            gained skills such as organization, the ability to provide and
+            receive feedback, my endless hunger to grow and collaboration, I can
+            be the perfect fit for your team!
           </p>
+          <br />
+          <p>My intention is to bring my diverse background and set of skills to the development and improvement of digital products, bringing creativity and efficiency together. </p>
         </div>
       </div>
     </div>
